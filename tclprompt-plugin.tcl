7acd48871d178985740ca8cbd5e6a2ec5fa0b816
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-11-03 21:01:06 +0100

    minimal plugin to (re)enable the tclprompt

---
+++ tclprompt-plugin.tcl
@@ -0,0 +1,8 @@
+# META TCL/TK prompt plugin
+# META DESCRIPTION enables a wee Tcl/Tk cmdline within the Pd-console
+# META AUTHOR IOhannes m zmölnig <zmoelnig@umlaeute.mur.at>
+# META VERSION 0.1
+
+package require pdwindow 0.1
+
+::pdwindow::create_tcl_entry
