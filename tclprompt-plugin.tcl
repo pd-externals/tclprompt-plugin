9451c853e9312353dc85ff8ea201cb26c43fb9f6
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-11-21 22:47:34 +0100

    ISO-8859 to UTF-8

--- tclprompt-plugin.tcl
+++ tclprompt-plugin.tcl
@@ -1,6 +1,6 @@
 # META TCL/TK prompt plugin
 # META DESCRIPTION enables a wee Tcl/Tk cmdline within the Pd-console
-# META AUTHOR IOhannes m zmölnig <zmoelnig@umlaeute.mur.at>, Hans-Christoph Steiner <hans@eds.org>
+# META AUTHOR IOhannes m zmÃ¶lnig <zmoelnig@umlaeute.mur.at>, Hans-Christoph Steiner <hans@eds.org>
 # META VERSION 0.1
 
 package require pdwindow 0.1
